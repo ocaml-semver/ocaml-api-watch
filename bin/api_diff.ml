3e90552e858c07769e465b8eb3a95f09164f6157
Nathan Rebours <nathan.p.rebours@gmail.com>
2024-02-23 10:48:42 +0100

    Upgrade to cmdliner 1.1.0
    Signed-off-by: Nathan Rebours <nathan.p.rebours@gmail.com>

--- bin/api_diff.ml
+++ bin/api_diff.ml
@@ -6,12 +6,9 @@ let run (`Ref_cmi reference) (`Current_cmi current) =
     Includemod.signatures typing_env ~mark:Mark_both reference.cmi_sign
       current.cmi_sign
   in
-  let () =
-    match coercion with
-    | Tcoerce_none -> Printf.printf "API unchanged!\n"
-    | _ -> Printf.printf "API changed!\n"
-  in
-  0
+  match coercion with
+  | Tcoerce_none -> Printf.printf "API unchanged!\n"
+  | _ -> Printf.printf "API changed!\n"
 
 let named f = Cmdliner.Term.(app (const f))
 
@@ -30,12 +27,12 @@ let current_cmi =
     Cmdliner.Arg.(required & pos 1 (some file) None & info ~doc ~docv [])
 
 let info =
-  Cmdliner.Term.info "api-watcher" ~version:"%%VERSION%%"
-    ~exits:Cmdliner.Term.default_exits
+  let open Cmdliner in
+  Cmd.info "api-watcher" ~version:"%%VERSION%%" ~exits:Cmd.Exit.defaults
     ~doc:"List API changes between two versions of a library"
 
 let term = Cmdliner.Term.(const run $ ref_cmi $ current_cmi)
 
-let main = (term, info)
+let main = Cmdliner.Cmd.v info term
 
-let () = Cmdliner.Term.(exit_status @@ eval main)
+let () = Stdlib.exit @@ Cmdliner.Cmd.eval main
