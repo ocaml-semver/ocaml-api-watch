69d67be5468c9468ec3d5f75fcacd57f34d0999c
munga <mwachilumobm@gmail.com>
2024-03-18 11:24:44 +0300

    Format test file

--- tests/api-watch-diff/test.ml
+++ tests/api-watch-diff/test.ml
@@ -8,40 +8,43 @@ open Types
 
 let%expect_test "Initial module signature test" =
   let mod_ref_signature : signature =
-    [ Sig_module (
-        Ident.create_persistent "M",
-        Mp_present,
-        {
-          md_type = Mty_signature [
-            Sig_value (
-              Ident.create_persistent "x",
-              {
-                val_type =
-                  Transient_expr.type_expr
-                    (Transient_expr.create (
-                      Tconstr (Path.Pident (Ident.create_persistent "int"), [], ref Mnil))
-                        ~level:0 ~scope:0 ~id:0
-                    );
-                val_kind = Val_reg;
-                val_attributes = [];
-                val_loc = Location.none;
-                val_uid = Uid.internal_not_actually_unique;
-              },
-              Exported
-            )
-          ];
-          md_attributes = [];
-          md_loc = Location.none;
-          md_uid = Uid.internal_not_actually_unique;
-        },
-        Trec_not,
-        Exported
-      )
+    [
+      Sig_module
+        ( Ident.create_persistent "M",
+          Mp_present,
+          {
+            md_type =
+              Mty_signature
+                [
+                  Sig_value
+                    ( Ident.create_persistent "x",
+                      {
+                        val_type =
+                          Transient_expr.type_expr
+                            (Transient_expr.create
+                               (Tconstr
+                                  ( Path.Pident (Ident.create_persistent "int"),
+                                    [],
+                                    ref Mnil ))
+                               ~level:0 ~scope:0 ~id:0);
+                        val_kind = Val_reg;
+                        val_attributes = [];
+                        val_loc = Location.none;
+                        val_uid = Uid.internal_not_actually_unique;
+                      },
+                      Exported );
+                ];
+            md_attributes = [];
+            md_loc = Location.none;
+            md_uid = Uid.internal_not_actually_unique;
+          },
+          Trec_not,
+          Exported );
     ]
   in
   let result =
-    Api_watch_diff.diff_interface ~reference:mod_ref_signature ~current:mod_ref_signature
+    Api_watch_diff.diff_interface ~reference:mod_ref_signature
+      ~current:mod_ref_signature
   in
   Format.printf "%b" result;
   [%expect {|false|}]
-
