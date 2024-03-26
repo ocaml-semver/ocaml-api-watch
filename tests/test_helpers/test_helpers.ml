2f79ab24f8268c787f3c5fe22ad737ffe17fe634
munga <mwachilumobm@gmail.com>
2024-03-26 10:36:45 +0300

    Use descriptive naming conventions

--- tests/test_helpers/test_helpers.ml
+++ tests/test_helpers/test_helpers.ml
@@ -1,18 +1,15 @@
-(* Parses and tokenizes the contents of the .mli file*)
 let parse_interface content =
   let lexbuf = Lexing.from_string content in
   Parse.interface lexbuf
 
-(* Generate the Types.signature from the parsed and typedtreed interface *)
 let generate_signature intf =
-  let env =
+  let typing_env =
     Typemod.initial_env ~loc:Location.none ~initially_opened_module:None
       ~open_implicit_modules:[]
   in
-  let typed_tree = Typemod.type_interface env intf in
+  let typed_tree = Typemod.type_interface typing_env intf in
   typed_tree.sig_type
 
-(* Compile the .mli content and return the signature *)
 let compile_interface (content : string) : Types.signature =
   let intf = parse_interface content in
   let signature = generate_signature intf in
