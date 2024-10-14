780a3c097d259839a29b901c555122f4b1b42653
azzsal <alkurd.abdulaziz@gmail.com>
2024-10-14 15:50:34 +0300

    Change internal representation of sig_item_map to a record

--- lib/sig_item_map.ml
+++ lib/sig_item_map.ml
@@ -1,10 +1,11 @@
 open Types
 
-type t =
-  value_description String_map.t
-  * module_declaration String_map.t
-  * modtype_declaration String_map.t
-  * (type_declaration * Ident.t) String_map.t
+type t = {
+  values_map : value_description String_map.t;
+  modules_map : module_declaration String_map.t;
+  modtypes_map : modtype_declaration String_map.t;
+  types_map : (type_declaration * Ident.t) String_map.t;
+}
 
 type _ item_type =
   | Value : value_description item_type
@@ -13,18 +14,43 @@ type _ item_type =
   | Type : (type_declaration * Ident.t) item_type
 
 let empty : t =
-  (String_map.empty, String_map.empty, String_map.empty, String_map.empty)
+  {
+    values_map = String_map.empty;
+    modules_map = String_map.empty;
+    modtypes_map = String_map.empty;
+    types_map = String_map.empty;
+  }
 
-let add : type a. name:string -> a item_type -> a -> t -> t =
- fun ~name item_type item maps ->
-  match (item_type, maps) with
-  | Value, (values_map, modules_map, modtypes_map, types_map) ->
-      (String_map.add name item values_map, modules_map, modtypes_map, types_map)
-  | Module, (values_map, modules_map, modtypes_map, types_map) ->
-      (values_map, String_map.add name item modules_map, modtypes_map, types_map)
-  | Modtype, (values_map, modules_map, modtypes_map, types_map) ->
-      (values_map, modules_map, String_map.add name item modtypes_map, types_map)
-  | Type, (values_map, modules_map, modtypes_map, types_map) ->
-      (values_map, modules_map, modtypes_map, String_map.add name item types_map)
+let add (type a) ~name (item_type : a item_type) (item : a)
+    { values_map; modules_map; modtypes_map; types_map } =
+  match item_type with
+  | Value ->
+      {
+        values_map = String_map.add name item values_map;
+        modules_map;
+        modtypes_map;
+        types_map;
+      }
+  | Module ->
+      {
+        values_map;
+        modules_map = String_map.add name item modules_map;
+        modtypes_map;
+        types_map;
+      }
+  | Modtype ->
+      {
+        values_map;
+        modules_map;
+        modtypes_map = String_map.add name item modtypes_map;
+        types_map;
+      }
+  | Type ->
+      {
+        values_map;
+        modules_map;
+        modtypes_map;
+        types_map = String_map.add name item types_map;
+      }
 
 let diff = failwith "TODO"
