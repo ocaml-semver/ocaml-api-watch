543b46b4f4a93afa79bb27b308a36d92ba980cd9
NchamJosephMuam <nchamjoe@gmail.com>
2024-10-22 16:25:53 +0100

    remove redundant and add test for modtype removal

--- tests/api-watch/test_diff_modtpe_decl.ml
+++ tests/api-watch/test_diff_modtpe_decl.ml
@@ -1,23 +1,6 @@
 open Api_watch
 open Test_helpers
 
-let%expect_test "Module_type with value changes" =
-  let reference =
-    compile_interface
-      {|
-    module type M = sig
-      val g : int -> string
-    end
-  |}
-  in
-  let current = compile_interface {|
-    module type M = sig end
-  |} in
-  let result = Diff.interface ~module_name:"Main" ~reference ~current in
-  Format.printf "%a" pp_diff_option result;
-  [%expect
-    {| Some (Module Main: {Modified (Supported [ Module M: {Modified (Supported [ Value (g, Removed)])}])}) |}]
-
 let%expect_test "Modified module type" =
   let reference =
     compile_interface
@@ -66,3 +49,16 @@ let%expect_test "Module type addition" =
   Format.printf "%a" pp_diff_option result;
   [%expect
     {| Some (Module Main: {Modified (Supported [ Module_type M: Added])}) |}]
+
+let%expect_test "Module type removal" =
+  let reference =
+    compile_interface {|
+   module type P = sig val y : int list end
+    |}
+  in
+  let current = compile_interface {|
+    |} in
+  let result = Diff.interface ~module_name:"Main" ~reference ~current in
+  Format.printf "%a" pp_diff_option result;
+  [%expect
+    {| Some (Module Main: {Modified (Supported [ Module_type P: Removed])}) |}]
