63dbe9cacdd337f0392a18b232e7a0119bebf084
Siddhi-agg <siddhiagrawalcool@gmail.com>
2025-01-12 21:36:49 +0530

    Format the changes

--- lib/library.mli
+++ lib/library.mli
@@ -1,3 +1,5 @@
 val load_cmi : string -> (Types.signature * string, string) result
 val load_unwrapped : string -> (Types.signature String_map.t, string) result
-val load : main_module:string -> string -> (Types.signature String_map.t, string) result
+
+val load :
+  main_module:string -> string -> (Types.signature String_map.t, string) result
