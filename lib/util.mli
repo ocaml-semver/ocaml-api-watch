85c56c46d9a7f2fcda41c098396e0be04d899d25
azzsal <alkurd.abdulaziz@gmail.com>
2025-02-10 10:41:57 +0200

    Add doc strings to normalization utils

--- lib/util.mli
+++ lib/util.mli
@@ -1,7 +1,13 @@
 val normalize_type_decls :
-  Types.type_declaration ->
-  Types.type_declaration ->
+  reference:Types.type_declaration ->
+  current:Types.type_declaration ->
   Types.type_declaration * Types.type_declaration
+(** Rename the occurances of each corresponding pair of type parameters in
+    [reference] and [current] to a unique ti,
+    where 1 <= i <= Int.max(List.length [current], List.length [reference]) *)
 
 val normalized_type_params :
-  Types.type_expr list -> Types.type_expr list -> bool
+  reference:Types.type_expr list -> current:Types.type_expr list -> bool
+(** Return true if each pair of corresponding type paramters
+    in [reference] and [current]
+    have the same name, false otherwise *)
