5728c9de289fabf76b052b1008c5222a2ea6774f
azzsal <alkurd.abdulaziz@gmail.com>
2025-02-14 10:33:37 +0200

    Update diff representation

--- lib/diff.mli
+++ lib/diff.mli
@@ -1,42 +1,59 @@
-type ('item, 'diff) t = Added of 'item | Removed of 'item | Modified of 'diff
+type ('item, 'diff) entry =
+  | Added of 'item
+  | Removed of 'item
+  | Modified of 'diff
+
 type 'a atomic_modification = { reference : 'a; current : 'a }
+type 'item atomic_entry = ('item, 'item atomic_modification) entry
 
-type 'item atomic_t = ('item, 'item atomic_modification) t
+type ('item, 'diff) map = {
+  same : 'item String_map.t;
+  different : ('item, 'diff) entry String_map.t;
+}
 
-type ('same, 'different) maybe_changed =
-  | Same of 'same
-  | Different of 'different
+type ('same, 'change) maybe_changed = Same of 'same | Changed of 'change
 
 type 'same atomic_maybe_changed =
   ('same, 'same atomic_modification) maybe_changed
 
-type ('same, 'diff) option_ =
-  ('same option, ('same, 'diff) t) maybe_changed
+type 'same atomic_maybe_changed_entry =
+  ('same, 'same atomic_entry) maybe_changed
 
-type 'same atomic_option =
-  ('same, 'same atomic_modification) option_
+type ('same, 'diff) option_ = ('same option, ('same, 'diff) entry) maybe_changed
+type 'same atomic_option = ('same, 'same atomic_modification) option_
 
 type ('same, 'diff) atomic_variant =
   ('same, ('same atomic_maybe_changed, 'diff) maybe_changed) maybe_changed
 
 type ('same, 'diff) list_ = ('same list, 'diff list) maybe_changed
 
-type value = {
-  vname : string;
-  vdiff : Types.value_description atomic_t;
-}
-
-type type_ = {
-  tname : string;
-  tdiff : (Types.type_declaration, type_modification) t;
-}
-
 and type_modification = {
   type_kind : (Types.type_decl_kind, type_kind) atomic_variant;
   type_privacy : (Asttypes.private_flag, type_privacy) maybe_changed;
   type_manifest : Types.type_expr atomic_option;
-  type_params: (Types.type_expr, type_param) list_
+  type_params : (Types.type_expr, type_param) list_;
 }
+
+and type_kind =
+  | Record_tk of (Types.label_declaration, record_field) map
+  | Variant_tk of
+      ( Types.constructor_declaration,
+        (Types.constructor_declaration, cstr_args) atomic_variant )
+      map
+
+and record_field = {
+  type_expr : Types.type_expr atomic_modification;
+  mutable_flag : (Asttypes.mutable_flag, field_mutability) maybe_changed;
+}
+
+and field_mutability = Added_m | Removed_m
+
+and cstr_args =
+  | Record_c of (Types.label_declaration, record_field) map
+  | Tuple_c of
+      (Types.type_expr, Types.type_expr atomic_maybe_changed_entry) list_
+
+and type_privacy = Added_p | Removed_p
 
 and type_param = (Types.type_expr, type_param_diff) maybe_changed
 
@@ -44,57 +61,22 @@ and type_param_diff =
   | Added_tp of Types.type_expr
   | Removed_tp of Types.type_expr
 
-and type_privacy = Added_p | Removed_p
-
-and type_kind =
-  | Record_tk of record_field list
-  | Variant_tk of constructor_ list
-
-and record_field = {
-  rname : string;
-  rdiff : Types.label_declaration atomic_t;
-}
-
-and constructor_ = {
-  csname : string;
-  csdiff : (Types.constructor_declaration, constructor_modification) atomic_variant;
-}
-
-and constructor_modification =
-  | Record_c of record_field list
-  | Tuple_c of tuple_component list
-
-and tuple_component = Types.type_expr atomic_maybe_changed
-
-type class_ = {
-  cname : string;
-  cdiff : Types.class_declaration atomic_t;
-}
-
-and cltype = {
-  ctname : string;
-  ctdiff : Types.class_type_declaration atomic_t;
-}
-
 type module_ = {
   mname : string;
-  mdiff : (Types.module_declaration, signature_modification) t;
+  mdiff : (Types.module_declaration, signature_modification) entry;
 }
 
-and modtype = {
-  mtname : string;
-  mtdiff : (Types.modtype_declaration, signature_modification) t;
-}
-
-and signature_modification = Unsupported | Supported of sig_item list
+and signature_modification =
+  | Unsupported
+  | Supported of (Types.signature_item, sig_item) map
 
 and sig_item =
-  | Value of value
-  | Module of module_
-  | Type of type_
-  | Modtype of modtype
-  | Class of class_
-  | Classtype of cltype
+  | Value of Types.value_description atomic_entry
+  | Class of Types.class_declaration atomic_entry
+  | Classtype of Types.class_type_declaration atomic_entry
+  | Type of (Types.type_declaration, type_modification) entry
+  | Module of (Types.module_declaration, signature_modification) entry
+  | Modtype of (Types.modtype_declaration, signature_modification) entry
 
 val interface :
   module_name:string ->
