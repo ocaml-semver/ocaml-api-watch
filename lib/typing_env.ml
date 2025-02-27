ef79404458f61c979461c5b448beff21caf0fdfe
azzsal <alkurd.abdulaziz@gmail.com>
2025-02-27 02:15:43 +0200

    Remove type equality trick

--- lib/typing_env.ml
+++ lib/typing_env.ml
@@ -1,6 +1,6 @@
 open Types
 
-let extract_non_alias_types signature =
+let _extract_non_alias_types signature =
   List.fold_left
     (fun acc item ->
       match item with
@@ -9,42 +9,12 @@ let extract_non_alias_types signature =
       | _ -> acc)
     String_map.empty signature
 
-let set_type_equality ref_id ~ref_decl ~curr_decl =
-  if List.length ref_decl.type_params = List.length curr_decl.type_params then
-    let ref_path = Path.Pident ref_id in
-    {
-      curr_decl with
-      type_manifest =
-        Some
-          (Btype.newgenty (Tconstr (ref_path, curr_decl.type_params, ref Mnil)));
-    }
-  else curr_decl
-
-let set_type_equalities ~reference ~current =
-  let ref_non_alias_types = extract_non_alias_types reference in
-  List.map
-    (fun item ->
-      match item with
-      | Sig_type
-          ( curr_id,
-            ({ type_manifest = None; _ } as curr_decl),
-            rec_st,
-            visibility ) -> (
-          let name = Ident.name curr_id in
-          match String_map.find_opt name ref_non_alias_types with
-          | Some (ref_id, ref_decl) ->
-              let new_decl = set_type_equality ref_id ~ref_decl ~curr_decl in
-              Sig_type (curr_id, new_decl, rec_st, visibility)
-          | None -> item)
-      | _ -> item)
-    current
-
 let for_diff ~reference ~current =
-  let modified_current = set_type_equalities ~reference ~current in
   let env = Env.empty in
   let env = Env.in_signature true env in
   let env = Env.add_signature reference env in
-  Env.add_signature modified_current env
+  let env = Env.add_signature current env in
+  env
 
 let pp fmt t =
   let summary = Env.summary t in
